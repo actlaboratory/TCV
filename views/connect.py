64b5d6737d74a19c2233042236ec6818849c7031
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2020-03-21 21:27:54 +0900

    接続ダイアログを実装

---
+++ views/connect.py
@@ -0,0 +1,29 @@
+﻿# -*- coding: utf-8 -*-
+# 接続ダイアログ
+
+import wx
+import globalVars
+import views.ViewCreator
+from logging import getLogger
+from views.baseDialog import *
+
+class Dialog(BaseDialog):
+	def Initialize(self):
+		self.identifier="connectDialog"#このビューを表す文字列
+		self.log=getLogger(self.identifier)
+		self.log.debug("created")
+		super().Initialize(self.app.hMainView.hFrame,_("接続"))
+		self.InstallControls()
+		return True
+
+	def InstallControls(self):
+		"""いろんなwidgetを設置する。"""
+		self.creator=views.ViewCreator.ViewCreator(1,self.panel,self.sizer,wx.VERTICAL,20)
+		self.iText,self.static=self.creator.inputbox(_("接続先"),400)
+
+		self.creator=views.ViewCreator.ViewCreator(1,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_BOTTOM | wx.ALIGN_RIGHT)
+		self.bOk=self.creator.okbutton(_("ＯＫ"),None)
+		self.bCancel=self.creator.cancelbutton(_("キャンセル"),None)
+
+	def GetData(self):
+		return self.iText.GetLineText(0)
