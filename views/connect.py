c0387e2b766407de192ae0dff4231d7e79fbd3dc
Kochi <c011610813@edu.teu.ac.jp>
2020-05-05 17:51:43 +0900

    wxPython4.1で起動できない問題に暫定対応

--- views/connect.py
+++ views/connect.py
@@ -21,7 +21,7 @@ class Dialog(BaseDialog):
 		self.creator=views.ViewCreator.ViewCreator(0,self.panel,self.sizer,wx.VERTICAL,20)
 		self.iText,self.static=self.creator.inputbox(_("接続先"),400)
 
-		self.creator=views.ViewCreator.ViewCreator(1,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_BOTTOM | wx.ALIGN_RIGHT)
+		self.creator=views.ViewCreator.ViewCreator(1,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_RIGHT)
 		self.bOk=self.creator.okbutton(_("ＯＫ"),None)
 		self.bCancel=self.creator.cancelbutton(_("キャンセル"),None)
 
