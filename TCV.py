cc32b22b90770bf3379d565b0c8d93042270b6f0
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2020-06-19 23:31:34 +0900

    エラーが出た際にビープを鳴らし、ダイアログを表示し、エラー情報をファイルに書き込むように修正

--- TCV.py
+++ TCV.py
@@ -11,13 +11,32 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import app as application
 import constants
 import globalVars
+import winsound
+import simpleDialog
+import sys
+import pathlib
+import traceback
+import simpleDialog
 
 def main():
+	if os.path.exists("errorLog.txt"): os.remove("errorLog.txt")
 	app=application.Main()
 	globalVars.app=app
 	app.initialize()
 	app.MainLoop()
 	app.config.write()
 
+def exchandler(type, exc, tb):
+	winsound.Beep(1000, 1000)
+	msg=traceback.format_exception(type, exc, tb)
+	simpleDialog.errorDialog(str(msg))
+	f=open("errorLog.txt", "a")
+	f.writelines(msg)
+	f.close()
+
+
+
 #global schope
+sys.excepthook=exchandler
+
 if __name__ == "__main__": main()
