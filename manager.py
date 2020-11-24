cad167eb62a71998c091724672e329b91bff679b
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2020-11-24 22:48:29 +0900

    不要なprintを削除

--- manager.py
+++ manager.py
@@ -455,6 +455,7 @@ class manager:
 				if globalVars.app.config.getboolean("fx", "playTypingSound", True) == True:
 					self.playFx(globalVars.app.config["fx"]["typingSound"])
 		elif id == evtPlaystatus:
+			globalVars.app.say(str(self.livePlayer.getStatus()))
 			if self.livePlayer.getStatus() != PLAYER_STATUS_PLAYING | self.livePlayer.getStatus() != PLAYER_STATUS_LOADING:
 				self.stop()
 
@@ -468,9 +469,7 @@ class manager:
 			if self.connection.movieInfo["movie"]["hls_url"] == None:
 				simpleDialog.errorDialog(_("再生URLを取得できません。"))
 				return
-			print(self.connection.movieInfo["movie"]["hls_url"])
 			setSource = self.livePlayer.setSource(self.connection.movieInfo["movie"]["hls_url"])
-			print(setSource)
 			if setSource == False:
 				simpleDialog.errorDialog(_("再生に失敗しました。"))
 				return
