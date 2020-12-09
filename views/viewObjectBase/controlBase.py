43560b98b483317c00e7d81ef1ec40e7ed35f945
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2020-12-09 11:13:13 +0900

    不要なprintを削除

--- views/viewObjectBase/controlBase.py
+++ views/viewObjectBase/controlBase.py
@@ -11,7 +11,6 @@ class controlBase():
 	def hideScrollBar(self, orient=wx.VERTICAL):
 		assert orient in (wx.VERTICAL, wx.HORIZONTAL, wx.VERTICAL | wx.HORIZONTAL)
 		if orient & wx.VERTICAL==wx.VERTICAL:
-			print("s")
 			ctypes.windll.user32.ShowScrollBar(self.GetHandle(),1,0)
 		if orient & wx.HORIZONTAL==wx.HORIZONTAL:
 			ctypes.windll.user32.ShowScrollBar(self.GetHandle(),0,0)
