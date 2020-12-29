2c385ce0b3729b36fbc567658bcde10f41eb22e7
Kochi <c011610813@edu.teu.ac.jp>
2020-12-29 22:35:29 +0900

    キー名は大文字表記に統一し、キー設定におけるバリデーションの不具合を修正

--- defaultKeymap.py
+++ defaultKeymap.py
@@ -5,25 +5,25 @@
 defaultKeymap={}
 
 defaultKeymap["MAINVIEW"]={
-	"CONNECT":"ctrl+n",
-	"VIEW_HISTORY": "ctrl+h",
-	"VIEW_FAVORITES": "ctrl+i",
-	"DISCONNECT":"ctrl+w/ctrl+F4",
+	"CONNECT":"CTRL+N",
+	"VIEW_HISTORY": "CTRL+H",
+	"VIEW_FAVORITES": "CTRL+I",
+	"DISCONNECT":"CTRL+W/CTRL+F4",
 	"EXIT": "",
 	"PLAY": "F1",
 	"STOP": "F2",
 	"VOLUME_UP": "F3",
 	"VOLUME_DOWN": "F4",
 	"RESET_VOLUME": "",
-	"CHANGE_DEVICE": "f5",
+	"CHANGE_DEVICE": "F5",
 	"COPY_COMMENT": "",
 	"VIEW_COMMENT": "",
 	"REPLY2SELECTED_COMMENT": "",
 	"DELETE_SELECTED_COMMENT": "",
-	"REPLY2BROADCASTER": "ctrl+shift+r",
-	"VIEW_BROADCASTER": "ctrl+b",
+	"REPLY2BROADCASTER": "CTRL+SHIFT+R",
+	"VIEW_BROADCASTER": "CTRL+B",
 	"OPEN_LIVE": "",
-	"ADD_FAVORITES": "ctrl+d",
+	"ADD_FAVORITES": "CTRL+D",
 	"SETTING": "",
 	"SET_KEYMAP": "",
 	"SET_HOTKEY": "",
@@ -39,14 +39,14 @@ defaultKeymap["MAINVIEW"]={
 
 defaultKeymap["commentList"]={
 	"COPY_COMMENT": "CTRL+C",
-	"VIEW_COMMENT": "Alt+RETURN",
-	"REPLY2SELECTED_COMMENT": "ctrl+r",
+	"VIEW_COMMENT": "ALT+RETURN",
+	"REPLY2SELECTED_COMMENT": "CTRL+R",
 	"DELETE_SELECTED_COMMENT": "DELETE",
 	"POPUP_OPEN_COMMENT": "APPLICATIONS/SHIFT+F10"
 }
 
 defaultKeymap["commentBody"]={
-	"POST_COMMENT": "ctrl+RETURN"
+	"POST_COMMENT": "CTRL+RETURN"
 }
 
 defaultKeymap["userInfo"]= {
