13131aa229843681cfe2df38c1422d435de63298
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2021-01-01 01:59:11 +0900

    環境設定を保存した際のattributeError対策

--- manager.py
+++ manager.py
@@ -677,7 +677,7 @@ class manager:
 		return current + count >= 35
 
 	def refreshReplaceSettings(self):
-		if hasattr(self, "connection") == False or self.MainView.commentList == None:
+		if hasattr(self, "connection") == False or hasattr(self.MainView, "commentList") == False or self.MainView.commentList == None:
 			return
 		tmplst = deepcopy(self.connection.comments)
 		if len(tmplst) > self.MainView.commentList.GetItemCount():
