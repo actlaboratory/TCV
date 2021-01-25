8cd8a4aa45be6bac8ed0d28f0b481d686a74b068
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2021-01-25 13:34:30 +0900

    エラー対応を強化

--- twitcasting/getTypingUser.py
+++ twitcasting/getTypingUser.py
@@ -3,8 +3,32 @@
 
 import requests
 import datetime
+import constants
+from logging import getLogger
+import traceback
+import sys
+
+log = getLogger("%s.%s" %(constants.LOG_PREFIX, "twitcasting.getTypingUser"))
 
 def getTypingUser(user, myself):
-	req = requests.get("http://twitcasting.tv/streamchecker.php?u=" + user + "&v=999&myself=" + myself + "&islive=1&lastitemid=-1&__c=" + str(int(datetime.datetime.now().timestamp())) + "000").text
+	try:
+		req = requests.get("http://twitcasting.tv/streamchecker.php?u=" + user + "&v=999&myself=" + myself + "&islive=1&lastitemid=-1&__c=" + str(int(datetime.datetime.now().timestamp())) + "000").text
+	except:
+		log.error("Connection failed.")
+		log.error(traceback.format_exc())
+		if not hasattr(sys, "frozen"):
+			import winsound
+			winsound.Beep(1000, 1000)
+			traceback.print_exc()
+		return ""
 	result = req.split("\t")
-	return result[8]
+	try:
+		return result[8]
+	except:
+		log.error("Failed. data: %s" %result)
+		log.error(traceback.format_exc())
+		if not hasattr(sys, "frozen"):
+			import winsound
+			winsound.Beep(1000, 1000)
+			traceback.print_exc()
+		return ""
