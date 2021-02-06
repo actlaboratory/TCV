84a344158dc9dc62f118187ee2e11a2e6054edde
Kochi <c011610813@edu.teu.ac.jp>
2021-02-06 10:03:20 +0900

    #83 アイテム履歴ウィンドウのレイアウト修正

--- views/itemHistory.py
+++ views/itemHistory.py
@@ -4,7 +4,6 @@
 import wx
 import globalVars
 import views.ViewCreator
-from logging import getLogger
 from views.baseDialog import *
 
 class Dialog(BaseDialog):
@@ -19,11 +18,11 @@ class Dialog(BaseDialog):
 
 	def InstallControls(self):
 		"""いろんなwidgetを設置する。"""
-		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.VERTICAL,20,style=wx.EXPAND|wx.ALL,margin=20)
-		self.historyList, self.static = self.creator.listCtrl(_("アイテム履歴"), None, wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.BORDER_RAISED,sizerFlag=wx.EXPAND)
-		self.historyList.InsertColumn(0, _("ユーザ"),width=450)
-		self.historyList.InsertColumn(1, _("アイテム"),width=450)
+		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.VERTICAL,10,style=wx.EXPAND|wx.ALL,margin=20)
+		self.historyList, self.static = self.creator.listCtrl(_("アイテム履歴"), None, wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.BORDER_RAISED,sizerFlag=wx.EXPAND,size=(700,400))
+		self.historyList.InsertColumn(0, _("ユーザ"),width=300)
+		self.historyList.InsertColumn(1, _("アイテム"),width=380)
 		for i in globalVars.app.Manager.items:
 			self.historyList.Append([i["user"], i["item"]])
-		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_RIGHT|wx.ALL,margin=20)
+		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_RIGHT|wx.RIGHT|wx.BOTTOM,margin=10)
 		self.bOk=self.creator.okbutton(_("閉じる"),None)
