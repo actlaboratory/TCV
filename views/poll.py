8ad30eb475e8e16cf3c27ff32824ffafc5ae0db2
Kazto Kitabatake <kitabatake1013@outlook.jp>
2023-09-08 20:16:11 +0900

    fix typo

--- views/poll.py
+++ views/poll.py
@@ -11,7 +11,7 @@ import simpleDialog
 
 class Dialog(BaseDialog):
 	def __init__(self, accounts, question, answers, sec):
-		super().__init__("postItemDialog")
+		super().__init__("pollDialog")
 		self.accounts = accounts
 		self.question = question
 		self.answers = answers
@@ -29,13 +29,13 @@ class Dialog(BaseDialog):
 		self.text = self.creator.staticText(self.question)
 		self.comboBox, self.comboBoxLabel = self.creator.combobox(_("回答"), self.answers, state=0)
 		self.creator=views.ViewCreator.ViewCreator(self.viewMode,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_RIGHT|wx.ALL,margin=20)
-		self.bOk=self.creator.okbutton(_("アイテム投下"), self.onOkButton)
+		self.bOk=self.creator.okbutton("OK", self.onOkButton)
 		self.bCancel=self.creator.cancelbutton(_("閉じる"))
 		# timer
 		self.timer = wx.Timer(self.wnd)
 		self.wnd.Bind(wx.EVT_TIMER, self.onTimerEvent)
 		# 秒→ミリ秒変換、回答送信時の遅延を考慮して3秒前にはダイアログを閉じる
-		timerInterval = self.sec * 1000 - 3
+		timerInterval = (self.sec - 3) * 1000
 		self.timer.Start(timerInterval, True)
 
 	def onOkButton(self, event):
